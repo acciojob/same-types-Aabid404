05b3c140411b4b29a69eef2a16688e7330cc8e04
AABID DEVELOPER <150765769+Aabid404@users.noreply.github.com>
2025-05-23 14:29:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function isSameType(value1, value2) {
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
+console.log(isSameType(1, 3));           // true
+console.log(isSameType("hey", "hello")); // true
+console.log(isSameType(NaN, NaN));       // true
+console.log(isSameType("3", 3));         // false
+console.log(isSameType(undefined, null));// false
+console.log(isSameType(true, false));    // true
+
